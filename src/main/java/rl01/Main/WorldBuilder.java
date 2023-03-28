a82e1e9f4dbb05a7fdf9b569cefc04cfdd5afee8
Eduardo Garay <93251806+EduardoGaray@users.noreply.github.com>
2023-03-28 11:53:50 +0200

    Working on Vision

--- src/main/java/rl01/Main/WorldBuilder.java
+++ src/main/java/rl01/Main/WorldBuilder.java
@@ -1,9 +1,5 @@
 package rl01.Main;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class WorldBuilder {
 
 	private int width;
@@ -56,101 +52,9 @@ public class WorldBuilder {
 		}
 		return this;
 	}
-	
-	private WorldBuilder createRegions(){
-        regions = new int[width][height][depth];
-    
-        for (int z = 0; z < depth; z++){
-            for (int x = 0; x < width; x++){
-                for (int y = 0; y < height; y++){
-                   if (tiles[x][y][z] != Tile.WALL && regions[x][y][z] == 0){
-                       int size = fillRegion(nextRegion++, x, y, z);
-              
-                       if (size < 25)
-                           removeRegion(nextRegion - 1, z);
-                   }
-                }
-            }
-        }
-        return this;
-    }
-	
-	private void removeRegion(int region, int z){
-	    for (int x = 0; x < width; x++){
-	        for (int y = 0; y < height; y++){
-	            if (regions[x][y][z] == region){
-	                regions[x][y][z] = 0;
-	                tiles[x][y][z] = Tile.WALL;
-	            }
-	        }
-	    }
-	}
-	
-	private int fillRegion(int region, int x, int y, int z) {
-        int size = 1;
-        ArrayList<Point> open = new ArrayList<Point>();
-        open.add(new Point(x,y,z));
-        regions[x][y][z] = region;
-    
-        while (!open.isEmpty()){
-            Point p = open.remove(0);
 
-            for (Point neighbor : p.neighbors8()){
-                if (regions[neighbor.x][neighbor.y][neighbor.z] > 0
-                  || tiles[neighbor.x][neighbor.y][neighbor.z] == Tile.WALL)
-                    continue;
-
-                size++;
-                regions[neighbor.x][neighbor.y][neighbor.z] = region;
-                open.add(neighbor);
-            }
-        }
-        return size;
-    }
-	
-	public WorldBuilder connectRegions(){
-        for (int z = 0; z < depth-1; z++){
-            connectRegionsDown(z);
-        }
-        return this;
-    }
-	
-	private void connectRegionsDown(int z, int r1, int r2){
-        List<Point> candidates = findRegionOverlaps(z, r1, r2);
-    
-        int stairs = 0;
-        do{
-            Point p = candidates.remove(0);
-            tiles[p.x][p.y][z] = Tile.STAIRS_DOWN;
-            tiles[p.x][p.y][z+1] = Tile.STAIRS_UP;
-            stairs++;
-        }
-        while (candidates.size() / stairs > 250);
-    }
-	
-	public List<Point> findRegionOverlaps(int z, int r1, int r2) {
-        ArrayList<Point> candidates = new ArrayList<Point>();
-    
-        for (int x = 0; x < width; x++){
-         for (int y = 0; y < height; y++){
-             if (tiles[x][y][z] == Tile.FLOOR
-                  && tiles[x][y][z+1] == Tile.FLOOR
-                  && regions[x][y][z] == r1
-                  && regions[x][y][z+1] == r2){
-              candidates.add(new Point(x,y,z));
-             }
-         }
-        }
-    
-        Collections.shuffle(candidates);
-        return candidates;
-    }
-	
 	public WorldBuilder makeCaves() {
-        return randomizeTiles()
-             .smooth(8)
-             .createRegions()
-             .connectRegions();
-    }
-	
+		return randomizeTiles().smooth(8);
+	}
+
 }
